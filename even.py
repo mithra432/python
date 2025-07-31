a5a2384b4c9c3ed4411b4336105b8f5b49467a71
mithra432 <202500239@sigc.edu>
2025-07-31 15:59:08 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,5 @@
+1.start the algorithm
+2. take an  angorithm from the user
+3.use the modulus operator to check the remainder when the number divided by.
+4.if the remainder is 0,the number is even;othewise,it is odd
+5.end
