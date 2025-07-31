b4a7121d95960c6072ed061f4acca3726ad7b788
mithra432 <202500239@sigc.edu>
2025-07-31 15:51:07 +0530

    Create temperture.py

---
+++ temperture.py
@@ -0,0 +1,8 @@
+                                                                    centigrate to fathrenheit 
+1.start
+2.declare variables for celsics and fahrenheit
+3.accept temperature in celsics from the user.\
+4.use the formula:
+         fahrenhit=(celsics*9/5)+32
+5.display the temperture in fahrenhit
+6.end.
