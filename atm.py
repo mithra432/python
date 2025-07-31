d1ebf3e685b8859c59fb7bd37f823f5bfb007942
mithra432 <202500239@sigc.edu>
2025-07-31 15:53:46 +0530

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,21 @@
+print("select the services you went:")
+print("1.withdraw")
+print("2.deposit\n3.saving\n4.balance")
+service=int(intput("pls select."))
+pin=1234
+if service==1:
+   print('withdraw')
+   amount=int(input("Enter a amount:"))
+   pin=int(input("Enter a secret pin"))
+if pin=PIn:
+   print('collect your amount')
+ else:
+   print("incorrect pin")
+elif service==2:
+   print('deposit'0)
+elif service==3:
+   print('savings')
+elif service==4:
+   print('balance')
+else:
+   print('kindly select the above services')
