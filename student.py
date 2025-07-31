e85e08cd0c3f1fda74b50a4a529b424f77dc9751
mithra432 <202500239@sigc.edu>
2025-07-31 15:56:38 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,25 @@
+def greet (name):
+      print('welcome',name)
+greet("sineka")
+greet("sri")
+student_list=['sri','nisha','bushra']
+for student in student_list:
+    greet(student)def greet (name):
+      print('welcome',name)
+greet("sineka")
+greet("sri")
+student_list=['sri','nisha','bushra']
+for student in student_list:
+    greet(student)def greet (name):
+      print('welcome',name)
+greet("sineka")
+greet("sri")
+student_list=['sri','nisha','bushra']
+for student in student_list:
+    greet(student)def greet (name):
+      print('welcome',name)
+greet("sineka")
+greet("sri")
+student_list=['sri','nisha','bushra']
+for student in student_list:
+    greet(student)
